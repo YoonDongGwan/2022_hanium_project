2723aca44b7ac38a03a637bad3cca99b106d7395
yundonggwan <ydg3825@gmail.com>
2022-11-02 12:24:16 +0900

    매칭중-매칭완료-결제내역

--- app/src/main/java/com/hanium/SJnJH/FindingPeopleActivity.kt
+++ app/src/main/java/com/hanium/SJnJH/FindingPeopleActivity.kt
@@ -1,6 +1,9 @@
 package com.hanium.SJnJH
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import com.hanium.R
 
@@ -16,5 +19,10 @@ class FindingPeopleActivity : AppCompatActivity() {
             finish()
         }
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent: Intent = Intent(this, MatchingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
--- app/src/main/java/com/hanium/activities/MainActivity.kt
+++ app/src/main/java/com/hanium/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hanium.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
@@ -10,9 +11,16 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.hanium.R
+import com.hanium.RetrofitResponse
+import com.hanium.RetrofitService
 import com.hanium.SJnJH.LoginActivity
 import com.hanium.adapters.HomeRecyclerViewAdapter
 import com.hanium.adapters.HomeViewPagerAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewpager: ViewPager2
@@ -33,8 +41,27 @@ class MainActivity : AppCompatActivity() {
 
         val arrayList: ArrayList<Int> = arrayListOf(1,2,3,4,5,6,7,8,9,10)
 
-//        recyclerview.adapter = HomeRecyclerViewAdapter(arrayList)
-        recyclerview.layoutManager = GridLayoutManager(this, 2)
+
+        val retrofit = Retrofit.Builder().baseUrl("http://52.78.209.45:3000")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val service = retrofit.create(RetrofitService::class.java)
+        service.getBooks().enqueue(object : Callback<RetrofitResponse> {
+            override fun onResponse(call: Call<RetrofitResponse>, response: Response<RetrofitResponse>) {
+                if (response.isSuccessful){
+                    var result: RetrofitResponse? = response.body()
+                    val arrayList = result?.data
+                    recyclerview.adapter = HomeRecyclerViewAdapter(applicationContext, arrayList)
+                    recyclerview.layoutManager = GridLayoutManager(applicationContext, 2)
+                }
+            }
+
+            override fun onFailure(call: Call<RetrofitResponse>, t: Throwable) {
+                Log.d("state", "onFailure" + t.message.toString())
+            }
+
+        })
+//        recyclerview.adapter = HomeRecyclerViewAdapter(this, arrayList)
+//        recyclerview.layoutManager = GridLayoutManager(this, 2)
 
         viewpager.adapter = HomeViewPagerAdapter(arrayList)
         viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
