38bad21045aa025389689e4620451a5f77df7952
Ji Hwan <g1767g@naver.com>
2022-11-18 02:02:01 +0900

    김지환 없뎃

--- app/src/main/java/com/hanium/activities/PriceResultActivity.kt
+++ app/src/main/java/com/hanium/activities/PriceResultActivity.kt
@@ -19,7 +19,9 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.hanium.R
 import com.hanium.SJnJH.ItemData
+import com.hanium.SJnJH.MenuData
 import com.hanium.SJnJH.StateData
+import org.json.JSONObject
 
 class PriceResultActivity : AppCompatActivity() {
     lateinit var rv : RecyclerView
@@ -28,27 +30,40 @@ class PriceResultActivity : AppCompatActivity() {
 
     var stateArr : ArrayList<StateData> = ArrayList()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_price_result)
         rv = findViewById(R.id.rv)
         rv2 = findViewById(R.id.rv2)
         payBt = findViewById(R.id.payBt)
+        val matchNum = intent.getIntExtra("matchNum",0)
+        val location = intent.getStringExtra("location").toString()
+        val storeName2 = intent.getStringExtra("storeName2").toString()
+        var menuArr = intent.getParcelableArrayListExtra<MenuData>("menuArr")
 
-
+        getPeople(matchNum, location, storeName2)
 
         payBt.setOnClickListener(){
-            val userName = intent.getStringExtra("userName").toString()
-            val location = intent.getStringExtra("deliveryPlace").toString()
-            val storeName2 = intent.getStringExtra("storeName2").toString()
-            Log.d("dddd","msg:$userName,$storeName2,$location")
+//            var i = 0
+//            while(i<stateArr.size) {
+//                var temp = stateArr.get(i)
+//                var name = temp.name
+//                var state = temp.state
+//                Log.d("ddss","dsd: $name,$state")
+//                i++
+//            }
         }
 
-//        var adapter : MyRvAdapter = MyRvAdapter(this,stateArr)
-//        rv.adapter = adapter
-//        rv.layoutManager = LinearLayoutManager(this)
-//
-//        adapter.notifyDataSetChanged()
+        var adapter2 : MyRvAdapter2 = MyRvAdapter2(this@PriceResultActivity,menuArr!!)
+        rv2.adapter = adapter2
+        rv2.layoutManager = LinearLayoutManager(this@PriceResultActivity)
+
+        adapter2.notifyDataSetChanged()
+
+
+
+
 
 
 
@@ -72,7 +87,39 @@ class PriceResultActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(holder: Holder, position: Int) {
             holder.tv1.setText(arr.get(position).name)
-            holder.tv2.setText(arr.get(position).state)
+            if(arr.get(position).state.equals("before"))
+                holder.tv2.setText("결제대기")
+            else
+                holder.tv2.setText("결제완료")
+
+
+        }
+
+        inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+            var tv1: TextView = itemView!!.findViewById(R.id.tv1)
+            val tv2: TextView = itemView!!.findViewById(R.id.tv2)
+
+
+        }
+    }
+
+
+    inner class MyRvAdapter2(val context: Context, val arr: ArrayList<MenuData>) :
+        RecyclerView.Adapter<MyRvAdapter2.Holder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val view = LayoutInflater.from(context).inflate(R.layout.price, parent, false)
+
+            return Holder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return arr.size
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.tv1.setText(arr.get(position).menu)
+            holder.tv2.setText(arr.get(position).price.toString())
 
 
         }
@@ -90,18 +137,18 @@ class PriceResultActivity : AppCompatActivity() {
 
 
 
-    fun getPeople(userName : String, location : String,storeName2 : String){
+    fun getPeople(matchNum : Int, location : String,storeName2 : String){
         var url = "http://52.78.209.45:3000/offline/get_people"
         val requestQueue = Volley.newRequestQueue(this)
 
         val request: StringRequest = object : StringRequest(
-            Request.Method.GET, url,request,fail ) {
+            Request.Method.POST, url,request,fail ) {
 
             override fun getParams(): MutableMap<String, String> {
                 val params : MutableMap<String,String> = HashMap()
 
-                params.put("userName",userName)
-                params.put("title",location)
+                params.put("matchNum",matchNum.toString())
+                params.put("location",location)
                 params.put("storeName2",storeName2)
 
                 return params
@@ -113,7 +160,23 @@ class PriceResultActivity : AppCompatActivity() {
 
     var request = object  : Response.Listener<String> {
         override fun onResponse(response: String) {
+            var jsonObject =  JSONObject(response)
+            var jsonArray = jsonObject.getJSONArray("data")
+            var i = 0
+            while(i<jsonArray.length()) {
+                var tempArray = jsonArray.getJSONObject(i)
+                var name = tempArray.getString("name")
+                var state = tempArray.getString("state")
+                var tempState = StateData(name, state)
+                stateArr.add(tempState)
+                i++
+            }
 
+            var adapter : MyRvAdapter = MyRvAdapter(this@PriceResultActivity,stateArr)
+            rv.adapter = adapter
+            rv.layoutManager = LinearLayoutManager(this@PriceResultActivity)
+
+            adapter.notifyDataSetChanged()
         }
 
 
